c75f765eb95a0268cdfee04509bc2b5c4bc7042d
thomas.ellis21 <thomas.ellis21@600900B211DE403>
2018-03-07 16:14:39 +1100

    Added Asteroids

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Moving : MonoBehaviour
+    {
+        public float accceleration = 5f;
+        public float rotationSpeed = 3f;
+        public float maxVelocity = 5f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            LimitVelocity();
+        }
+        void LimitVelocity()
+        {
+            Vector3 vel =rigid.velocity;
+            if(vel.magnitude > maxVelocity)
+            {
+                vel = vel.normalized * maxVelocity;
+            }
+            rigid.velocity = vel; 
+        }
+
+        public void Accelerate(Vector3 direction)
+        {
+            rigid.AddForce(direction * accceleration);
+        }
+        public void RotateLeft()
+        {
+            rigid.rotation += rotationSpeed * Time.deltaTime;
+        }
+        public void RotateRight()
+        {
+            rigid.rotation -= rotationSpeed * Time.deltaTime;
+        }
+    }
+}
+
+
---
+++ Assets/~Asteroids/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Moving movement;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            // -1   == S || DownArrow
+            //  0   == Not Pressed
+            //  1   == W || UpArrow
+
+            float inputH = Input.GetAxis("Horizontal");
+            // -1   == A || LeftArrow
+            //  0   == Not Pressed
+            //  1   == D || RightArrow
+
+            if (inputV > 0)
+            {
+                movement.Accelerate(transform.up);
+            }
+
+            // If inputH > 0
+            if (inputH > 0)
+            {
+                // Rotate Right
+                movement.RotateRight();
+            }
+
+            // If inputH < 0
+                // Rotate Left
+        }
+    }
+}
